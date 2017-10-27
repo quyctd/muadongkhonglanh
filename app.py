54e72936afb4b7f1da3bfd2bf7f2c5a3c163c921
quyctd <skyvp98@gmail.com>
2017-10-27 18:56:46 +0700

    cmt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, redirect, url_for
 import mlab
 from mongoengine import *
 from faker import Faker
@@ -62,6 +62,37 @@ def remove(id):
     return render_template("admin.html", girls = girllist_after_delete)
 
 
+@app.route('/delete-girl/<girl_id>')
+def delete_girl(girl_id):
+    girl = Girl.objects().with_id(girl_id)
+    if girl is None:
+        print("not found")
+    else:
+        girl.delete()
+    return "Delete "+girl_id
+
+@app.route('/update-girl/<girl_id>', methods=['GET', 'POST'])
+def update(girl_id):
+    if request.method == "GET":
+        girl = Girl.objects().with_id(girl_id)
+
+        return render_template('update_girl.html', girl = girl)
+    elif request.method == "POST":
+        girl = Girl.objects().with_id(girl_id)
+        form = request.form
+        girl.name = form['name']
+        girl.image = form['image']
+        girl.description = form['description']
+
+
+        girl.save()
+        #Cách khác
+        #name = form['name']
+        #image = form['image']
+        #description = form['description']
+        #girl.update(set__name = name, set__image = image, set__description == description)
+
+        return redirect(url_for('admin'))
 
 # @app.route('/girl')
 # def index_girl():
